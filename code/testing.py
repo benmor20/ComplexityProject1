2fe8c2c9fb8dd91e735e09f3757f527e0151235a
benmor20 <benmor2020@gmail.com>
2021-10-26 11:35:13 -0400

    P Hetero by Threshold

--- code/testing.py
+++ code/testing.py
@@ -4,22 +4,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-radii = [1, 4, 7]
+phs = [0, 0.4, 0.8]
 steps = (0, 500, 1000)
 ts = [0.2, 0.3, 0.4]
 
-for ri, r in enumerate(radii):
+for phi, ph in enumerate(phs):
     for ti, t in enumerate(ts):
-        city = City(50, r=r)
+        city = HomoHeteroCity(50, r=4, hetero_prob=ph)
         city.loop_until_done(threshold=t)
-        index = ri * len(radii) + ti + 1
-        plt.subplot(len(radii), len(ts), index)
+        index = phi * len(phs) + ti + 1
+        plt.subplot(len(phs), len(ts), index)
         city.draw(plot_show=False)
-        if index == ri * len(radii) + 1:
-            plt.ylabel(f'Radius = {r}')
-        if index > len(radii) * (len(radii) - 1):
+        if index == phi * len(phs) + 1:
+            plt.ylabel(f'% Hetero = {ph}')
+        if index > len(phs) * (len(phs) - 1):
             plt.xlabel(f'Threshold = {t}')
         if index == len(ts) // 2 + 1:
             plt.title(f'Equilibrium States for Varying Cities')
-        print(f'Finished {t}, {r}')
+        print(f'Finished {t}, {ph}')
 plt.show()
