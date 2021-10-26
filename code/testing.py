84b79c33e240f90758f020ea51aa5d8fd14a641e
benmor20 <benmor2020@gmail.com>
2021-10-25 21:59:20 -0400

    3x3 plot varying radius and threshold

--- code/testing.py
+++ code/testing.py
@@ -8,22 +8,18 @@ radii = [1, 4, 7]
 steps = (0, 500, 1000)
 ts = [0.2, 0.3, 0.4]
 
-for t in ts:
-    for ri, r in enumerate(radii):
+for ri, r in enumerate(radii):
+    for ti, t in enumerate(ts):
         city = City(50, r=r)
-        current_steps = 0
-        for si, step in enumerate(steps):
-            while current_steps < step:
-                city.step(threshold=t)
-                current_steps += 1
-            index = ri * len(steps) + si + 1
-            plt.subplot(len(radii), len(steps), index)
-            if index == ri * len(steps) + 1:
-                plt.ylabel(f'Radius = {r}')
-            if index > (len(radii) - 1) * len(steps):
-                plt.xlabel(f'Steps = {step}')
-            if index == len(steps) // 2 + 1:
-                plt.title(f'Segregation of Cities with Differing Vision, threshold={t}')
-            city.draw(plot_show=False)
+        city.loop_until_done(threshold=t)
+        index = ri * len(radii) + ti + 1
+        plt.subplot(len(radii), len(ts), index)
+        city.draw(plot_show=False)
+        if index == ri * len(radii) + 1:
+            plt.ylabel(f'Radius = {r}')
+        if index > len(radii) * (len(radii) - 1):
+            plt.xlabel(f'Threshold = {t}')
+        if index == len(ts) // 2 + 1:
+            plt.title(f'Equilibrium States for Varying Cities')
         print(f'Finished {t}, {r}')
-    plt.show()
+plt.show()
